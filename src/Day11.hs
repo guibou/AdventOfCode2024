92798d0ae370733465547e556dc16da7abb5de96
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2024-12-12 19:19:16 +0400

    Day11: avoid read/show and avoid lists

--- src/Day11.hs
+++ src/Day11.hs
@@ -4,24 +4,23 @@ import Utils
 
 fileContent = [0 :: Int, 44, 175060, 3442, 593, 54398, 9, 8101095]
 
-blink 0 = [1]
-blink x
-  | even ds = [read $ take (ds `div`2) digits, read $ drop (ds `div` 2) digits]
-  | otherwise = [x * 2024]
-  where
-    digits = show x
-    ds = length digits
-
 blinksFix :: (Int -> Int -> Int) -> Int -> Int -> Int
 blinksFix _ 0 _ = 1
-blinksFix f n x = do
-  sum $ do
-    x' <- blink x
-    pure $ f (n - 1) x'
+blinksFix f n 0 = f (n - 1) 1
+blinksFix f n x
+  | even ds = f (n - 1) a + f (n - 1) b
+  | otherwise = f (n - 1) (x * 2024)
+  where
+    ds = nbDigits x
+    (a, b) = x `divMod` (10 ^ (ds `div` 2))
+
+nbDigits :: Int -> Int
+nbDigits x = go x 0
+  where
+    go 0 !n = n
+    go v n = go (v `div` 10) (n + 1)
 
 blinks = memoFix2 blinksFix
-
-parseContent = undefined
 
 -- * Generics
 
