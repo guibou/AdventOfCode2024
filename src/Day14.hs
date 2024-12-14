1d11c672bc34eb208f6760af47a5e87671dcd970
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2024-12-14 16:50:03 +0400

    Day14: bench (for star 1). I have no idea what to do on star 2 ;)

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,17 +1,24 @@
 module Day14 where
 
-import Utils
-import GHC.Generics
 import Control.Applicative (some)
-import qualified Data.Map.Strict as Map
+import Data.Map.Strict qualified as Map
+import Data.Text qualified as Text
+import GHC.Generics
+import Utils
+import Control.Monad (when)
+import qualified Data.Set as Set
+import Data.List (find)
+import Debug.Trace
 
-data Robot = Robot {
-  p :: V2 Int,
-  v :: V2 Int
- }
- deriving (Show, Generic)
+data Robot = Robot
+  { p :: V2 Int,
+    v :: V2 Int
+  }
+  deriving (Show, Generic)
 
-fileContent = parseContent $(getFile)
+fileBounds = V2 101 103 :: V2 Int
+
+fileContent = (fileBounds, parseContent $(getFile))
 
 parseContent = unsafeParse $ some parseRobot
 
@@ -28,21 +35,21 @@ parseRobot = do
   pure $ Robot {p = V2 px py, v = V2 vx vy}
 
 -- * Generics
+
 stepRobot :: V2 Int -> Robot -> Robot
 -- Note: this could be done instantly by multipling the speed by N
-stepRobot bounds r = r { p = (r.p + r.v) `modV2` bounds }
+stepRobot bounds r = r {p = (r.p + r.v) `modV2` bounds}
 
 (V2 a b) `modV2` (V2 x y) = V2 (a `mod` x) (b `mod` y)
+
 (V2 a b) `divV2` (V2 x y) = V2 (a `div` x) (b `div` y)
 
 -- * FIRST problem
-fileBounds = V2 101 103 :: V2 Int
-exBounds = V2 11 7 :: V2 Int
 
-stepRobots bounds = map (applyN 100 (stepRobot bounds))
+stepRobots n bounds = map (applyN n  (stepRobot bounds))
 
-day robots bounds = do
-  let robots' = map (applyN 100 (stepRobot bounds)) robots
+day (bounds, robots) = do
+  let robots' = stepRobots 100 bounds robots
   let robotsCounts = Map.fromListWith (+) $ do
         r <- robots'
         let middle = bounds `divV2` 2
@@ -53,10 +60,61 @@ day robots bounds = do
         pure (V2 (if r.p.x < middle.x then 0 else 1) (if r.p.y < middle.y then 0 else 1), 1)
   product robotsCounts
 
+robotCounts (bounds, robots) = Map.fromListWith (+) $ do
+  r <- robots
+  pure (r.p, 1)
+
 -- * SECOND problem
+
 day' = undefined
 
-ex = parseContent [str|\
+displayRobots (bounds, robots') steps = do
+  let robots = stepRobots steps bounds robots'
+  let boundsMap = Map.fromList $ do
+        y <- [0 .. bounds.y]
+        x <- [0 .. bounds.x]
+
+        pure (V2 x y, " ")
+  display2DGrid (fmap (Text.pack . show) (robotCounts (bounds, robots)) <> boundsMap)
+
+displayRobots' (bounds, robots) = do
+  let boundsMap = Map.fromList $ do
+        y <- [0 .. bounds.y]
+        x <- [0 .. bounds.x]
+
+        pure (V2 x y, ".")
+
+  let go i robots = do
+        putStrLn $ "--- " <> show i
+        when (checkConnexity robots) $ do
+          display2DGrid (fmap (Text.pack . show) (robotCounts (bounds, robots)) <> boundsMap)
+          _ <- getLine
+          pure ()
+        go (i + 1) (stepRobots 1 bounds robots)
+  go 0 robots
+
+findEsterEgg (bounds, robots) = do
+  let allRobots = iterate (map (stepRobot bounds)) robots
+      treeSet = buildChritmasTree bounds
+  traceShow (length treeSet, length robots) $ find (\(i, robots) -> Set.fromList (map (\r -> r.p) robots) == treeSet) $ zip [0..] allRobots
+
+exBounds = V2 11 7 :: V2 Int
+
+buildChritmasTree :: V2 Int -> Set (V2 Int)
+buildChritmasTree bounds = Set.fromList $ do
+          y <- [0 .. bounds.y - 1]
+          x <- [bounds.x `div` 2 - y, bounds.x `div` 2 + y]
+          pure $ V2 x y
+
+checkConnexity robots = all hasFriend robots
+  where
+    robotsPos = Set.fromList $ map (.p) robots
+    hasFriend r = any (\p -> p `Set.member` robotsPos) $ (map (r.p+) (drop 1 connect8))
+
+ex =
+  ( exBounds,
+    parseContent
+      [str|\
 p=0,4 v=3,-3
 p=6,3 v=-1,-3
 p=10,3 v=-1,2
@@ -70,5 +128,6 @@ p=7,3 v=-1,2
 p=2,4 v=2,-3
 p=9,5 v=-3,-3
 |]
+  )
 
 -- started at Sat Dec 14 03:36:15 PM +04 2024
