6a2b3ad52e4ce80774cc116e0dc4789092a2aa21
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2024-12-19 14:41:13 +0400

    Day19: optimisation: shortcircuit for first logic

--- src/Day19.hs
+++ src/Day19.hs
@@ -14,19 +14,37 @@ parseContent t = do
 -- * Generics
 
 -- * FIRST problem
-day (towels, designs) = length $ filter (\design -> validateDesign towels design > 0) designs
-
-validateDesign :: [String] -> String -> Int
-validateDesign towels = memoFix goFix
+day (towels, designs) = length $ filter count designs
   where
-    goFix _ "" = 1
-    goFix f design = sum $ do
+    count = countDesign towels
+
+class OneSum t where
+  one :: t
+  reduce :: [t] -> t
+
+instance OneSum Bool where
+  one = True
+  -- This shortcut, so the first valid result stops the computation
+  reduce = or
+
+instance OneSum Int where
+  one = 1
+  reduce = sum
+
+countDesign :: OneSum t => [String] -> String -> t
+countDesign towels = memoFix goFix
+  where
+    goFix _ "" = one
+    goFix f design = reduce $ do
       towel <- towels
       guard $ towel `isPrefixOf` design
       pure $ f (drop (length towel) design)
 
 -- * SECOND problem
-day' (towels, designs) = sum $ map (\design -> validateDesign towels design) designs
+day' (towels, designs) = sum $ map validate designs
+  where
+    -- Share the cache between ALL invocations
+    validate = countDesign @Int towels
 
 ex = parseContent [str|\
 r, wr, b, g, bwu, rb, gb, br
