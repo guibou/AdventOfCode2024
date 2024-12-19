93290024758824ed2ad24eb5b2a8053aed084f6c
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2024-12-19 12:08:11 +0400

    Day17 first star

---
+++ src/Day17.hs
@@ -0,0 +1,100 @@
+module Day17 where
+
+import Utils
+import qualified Data.Vector as Vector
+import Data.Bits (xor)
+import Data.List
+import Debug.Trace
+
+data Computer = Computer {
+   registerA :: Int,
+   registerB :: Int,
+   registerC :: Int,
+   program :: Vector Int,
+   pc :: Int
+   }
+    deriving (Show)
+
+fileContent = Computer {
+   registerA = 30878003,
+   registerB = 0,
+   registerC = 0,
+
+   program = Vector.fromList [2,4,1,2,7,5,0,3,4,7,1,7,5,5,3,0],
+   pc = 0
+   }
+
+-- * Generics
+
+-- * FIRST problem
+run :: Computer -> [Int]
+run computer
+  | computer.pc + 1 >= length computer.program = []
+  | otherwise = do
+    let
+       instruction = computer.program Vector.! computer.pc
+       operand = computer.program Vector.! (computer.pc + 1)
+
+    case instruction of
+      0 -> do -- adv
+        let numerator = computer.registerA
+        let denominator = 2 ^ (combo computer operand)
+        run (computer { registerA = numerator `div` denominator, pc = computer.pc + 2 })
+      1 -> do -- bxl
+        run (computer { registerB = computer.registerB `xor` operand, pc = computer.pc + 2 })
+      2 -> do -- bst
+        run (computer { registerB = combo computer operand `mod` 8, pc = computer.pc + 2 })
+      3 -> do -- jnz
+        if computer.registerA == 0
+        then run (computer { pc = computer.pc + 2 })
+        else run (computer { pc = operand })
+      4 -> do -- bxc
+        run (computer { registerB = computer.registerB `xor` computer.registerC, pc = computer.pc + 2 })
+      5 -> do -- out
+        (combo computer operand `mod` 8) : run (computer { pc = computer.pc + 2 })
+      6 -> do -- bdv
+        let numerator = computer.registerA
+        let denominator = 2 ^ (combo computer operand)
+        run (computer { registerB = numerator `div` denominator, pc = computer.pc + 2 })
+      7 -> do -- cdv
+        let numerator = computer.registerA
+        let denominator = 2 ^ (combo computer operand)
+        run (computer { registerC = numerator `div` denominator, pc = computer.pc + 2 })
+
+day computer = intercalate "," (map show $ run computer)
+
+        
+    
+combo :: Computer -> Int -> Int
+combo computer val = case val of
+  0 -> 0
+  1 -> 1
+  2 -> 2
+  3 -> 3
+  4 -> computer.registerA
+  5 -> computer.registerB
+  6 -> computer.registerC
+  7 -> error "reserved"
+
+-- * SECOND problem
+day' computer = find (\newA -> run (computer { registerA = newA }) == program) [0..]
+  where
+    program = Vector.toList computer.program
+
+ex = Computer {
+   registerA = 729,
+   registerB = 0,
+   registerC = 0,
+   program = Vector.fromList [0,1,5,4,3,0],
+   pc = 0
+}
+
+ex' = Computer { 
+  registerA = 2024,
+  registerB = 0,
+  registerC = 0,
+  program = Vector.fromList [0,3,5,4,3,0],
+  pc = 0
+}
+
+-- started at Thu Dec 19 11:40:12 AM +04 2024
---
+++ tests/Day17Spec.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+module Day17Spec where
+
+import Day17
+import Test.Syd
+
+-- We default to Int for performance, if Integer is required, it will be force typed on the different Day
+default (Int)
+
+spec :: Spec
+spec = do
+  describe "simple examples" $ do
+    it "of first star" $ do
+      day ex `shouldBe` "4,6,3,5,6,3,5,2,1,0"
+    it "of second star" $ do
+      day' ex' `shouldBe` Just 117440
+  describe "works" $ do
+    it "on first star" $ do
+      day fileContent `shouldBe` "7,1,3,7,5,1,0,3,4"
+    it "on second star" $ do
+      day' fileContent `shouldBe` Just 0
